ffca8a818d7a0c996da439d0ae623b512c520c2e
velaz <juanma261005@gmail.com>
2023-07-10 18:00:51 -0400

    use of lists to store the pages instead of multiple variables

--- app/src/main/java/com/jjgn/app/devlearn/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/jjgn/app/devlearn/viewmodel/AppViewModel.kt
@@ -47,35 +47,37 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
 
     val textSize = MutableStateFlow(16)
 
-    val ktm1p = MutableStateFlow(1)
-    val ktm1 = MutableStateFlow(0)
-    val ktm2p = MutableStateFlow(1)
-    val ktm2 = MutableStateFlow(0)
-    val ktm3p = MutableStateFlow(1)
-    val ktm3 = MutableStateFlow(0)
+    var cModulesCurrentPage = mutableListOf(
+        1,// KTM1 0
+        1,// KTM2 1
+        1,// KTM3 2
+        1,// JVM1 3
+        1,// JVM2 4
+        1,// JVM3 5
+        1,// JSM1 6
+        1,// JSM2 7
+        1,// JSM3 8
+        1,// PYM1 9
+        1,// PYM2 10
+        1,// PYM3 11
+        1 // NM 12
+    )
 
-    val jvm1p = MutableStateFlow(1)
-    val jvm1 = MutableStateFlow(0)
-    val jvm2p = MutableStateFlow(1)
-    val jvm2 = MutableStateFlow(0)
-    val jvm3p = MutableStateFlow(1)
-    val jvm3 = MutableStateFlow(0)
-
-    val jsm1p = MutableStateFlow(1)
-    val jsm1 = MutableStateFlow(0)
-    val jsm2p = MutableStateFlow(1)
-    val jsm2 = MutableStateFlow(0)
-    val jsm3p = MutableStateFlow(1)
-    val jsm3 = MutableStateFlow(0)
-
-    val pym1p = MutableStateFlow(1)
-    val pym1 = MutableStateFlow(0)
-    val pym2p = MutableStateFlow(1)
-    val pym2 = MutableStateFlow(0)
-    val pym3p = MutableStateFlow(1)
-    val pym3 = MutableStateFlow(0)
-
-    val nm = MutableStateFlow(0)
+    val cModulesTPages = mutableListOf(
+        10,// KTM1 0
+        10,// KTM2 1
+        10,// KTM3 2
+        10,// JVM1 3
+        10,// JVM2 4
+        10,// JVM3 5
+        10,// JSM1 6
+        10,// JSM2 7
+        10,// JSM3 8
+        10,// PYM1 9
+        10,// PYM2 10
+        10,// PYM3 11
+        10 // NM 12
+    )
 
     /**
      * Funcion iniciador. Obtiene el contexto desde [com.jjgn.app.devlearn.ui.components.Content]
@@ -119,18 +121,18 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
             _currentMState
         )
         _currentPage.value = when (_currentMState.value) {
-            is Module.KTM1 -> ktm1p.value
-            is Module.KTM2 -> ktm2p.value
-            is Module.KTM3 -> ktm3p.value
-            is Module.JVM1 -> jvm1p.value
-            is Module.JVM2 -> jvm2p.value
-            is Module.JVM3 -> jvm3p.value
-            is Module.JSM1 -> jsm1p.value
-            is Module.JSM2 -> jsm2p.value
-            is Module.JSM3 -> jsm3p.value
-            is Module.PYM1 -> pym1p.value
-            is Module.PYM2 -> pym2p.value
-            is Module.PYM3 -> pym3p.value
+            is Module.KTM1 -> cModulesCurrentPage[0]
+            is Module.KTM2 -> cModulesCurrentPage[1]
+            is Module.KTM3 -> cModulesCurrentPage[2]
+            is Module.JVM1 -> cModulesCurrentPage[3]
+            is Module.JVM2 -> cModulesCurrentPage[4]
+            is Module.JVM3 -> cModulesCurrentPage[5]
+            is Module.JSM1 -> cModulesCurrentPage[6]
+            is Module.JSM2 -> cModulesCurrentPage[7]
+            is Module.JSM3 -> cModulesCurrentPage[8]
+            is Module.PYM1 -> cModulesCurrentPage[9]
+            is Module.PYM2 -> cModulesCurrentPage[10]
+            is Module.PYM3 -> cModulesCurrentPage[11]
             else -> _currentPage.value
         }
     }
@@ -138,18 +140,18 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
     // Funcion que se encarga de ir a la siguiente pagina
     fun nextPage() {
         when (_currentMState.value) {
-            is Module.KTM1 -> ktm1p.value++
-            is Module.KTM2 -> ktm2p.value++
-            is Module.KTM3 -> ktm3p.value++
-            is Module.JVM1 -> jvm1p.value++
-            is Module.JVM2 -> jvm2p.value++
-            is Module.JVM3 -> jvm3p.value++
-            is Module.JSM1 -> jsm1p.value++
-            is Module.JSM2 -> jsm2p.value++
-            is Module.JSM3 -> jsm3p.value++
-            is Module.PYM1 -> pym1p.value++
-            is Module.PYM2 -> pym2p.value++
-            is Module.PYM3 -> pym3p.value++
+            is Module.KTM1 -> cModulesCurrentPage[0]++
+            is Module.KTM2 -> cModulesCurrentPage[1]++
+            is Module.KTM3 -> cModulesCurrentPage[2]++
+            is Module.JVM1 -> cModulesCurrentPage[3]++
+            is Module.JVM2 -> cModulesCurrentPage[4]++
+            is Module.JVM3 -> cModulesCurrentPage[5]++
+            is Module.JSM1 -> cModulesCurrentPage[6]++
+            is Module.JSM2 -> cModulesCurrentPage[7]++
+            is Module.JSM3 -> cModulesCurrentPage[8]++
+            is Module.PYM1 -> cModulesCurrentPage[9]++
+            is Module.PYM2 -> cModulesCurrentPage[10]++
+            is Module.PYM3 -> cModulesCurrentPage[11]++
             else -> return
         }
         if (_currentPage.value < totalPages) {
@@ -161,18 +163,18 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
     // Funcion que permite retroceder de pagina.
     fun prevPage() {
         when (_currentMState.value) {
-            is Module.KTM1 -> ktm1p.value--
-            is Module.KTM2 -> ktm2p.value--
-            is Module.KTM3 -> ktm3p.value--
-            is Module.JVM1 -> jvm1p.value--
-            is Module.JVM2 -> jvm2p.value--
-            is Module.JVM3 -> jvm3p.value--
-            is Module.JSM1 -> jsm1p.value--
-            is Module.JSM2 -> jsm2p.value--
-            is Module.JSM3 -> jsm3p.value--
-            is Module.PYM1 -> pym1p.value--
-            is Module.PYM2 -> pym2p.value--
-            is Module.PYM3 -> pym3p.value--
+            is Module.KTM1 -> cModulesCurrentPage[0]--
+            is Module.KTM2 -> cModulesCurrentPage[0]--
+            is Module.KTM3 -> cModulesCurrentPage[0]--
+            is Module.JVM1 -> cModulesCurrentPage[0]--
+            is Module.JVM2 -> cModulesCurrentPage[0]--
+            is Module.JVM3 -> cModulesCurrentPage[0]--
+            is Module.JSM1 -> cModulesCurrentPage[0]--
+            is Module.JSM2 -> cModulesCurrentPage[0]--
+            is Module.JSM3 -> cModulesCurrentPage[0]--
+            is Module.PYM1 -> cModulesCurrentPage[0]--
+            is Module.PYM2 -> cModulesCurrentPage[0]--
+            is Module.PYM3 -> cModulesCurrentPage[0]--
             else -> return
         }
         if (_currentPage.value > 1) {
@@ -186,18 +188,7 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
         totalPages = getTotalPages(
             _currentState,
             _currentMState,
-            ktm1,
-            ktm2,
-            ktm3,
-            jvm1,
-            jvm2,
-            jvm3,
-            jsm1,
-            jsm2,
-            jsm3,
-            pym1,
-            pym2,
-            pym3
+            cModulesTPages
         )
         langName = getLangName(_currentState)
         _infoString.value = getTextToShow(
@@ -216,6 +207,7 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
             editor.apply()
         }
     }
+
     private fun zoomStateRestorer(context: Context) {
         viewModelScope.launch(Dispatchers.IO) {
             preferences = context.getSharedPreferences(preferenceName, Context.MODE_PRIVATE)
@@ -223,76 +215,15 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
         }
     }
 
-    // Funcion encargada de guardar las paginas en la que el usuario se queda.
+    // Funcion encargada de guardar las paginas en las que el usuario se queda.
     fun dataSaver(context: Context) {
         viewModelScope.launch(Dispatchers.IO) {
             preferences = context.getSharedPreferences(preferenceName, Context.MODE_PRIVATE)
             val editor: SharedPreferences.Editor = preferences.edit()
-            when (_currentMState.value) {
-                is Module.KTM1 -> {
-                    editor.putInt("kotlinModule1Page", ktm1p.value)
-                    editor.apply()
-                }
-
-                is Module.KTM2 -> {
-                    editor.putInt("kotlinModule2Page", ktm2p.value)
-                    editor.apply()
-                }
-
-                is Module.KTM3 -> {
-                    editor.putInt("kotlinModule3Page", ktm3p.value)
-                    editor.apply()
-                }
-
-                is Module.JVM1 -> {
-                    editor.putInt("javaModule1Page", jvm1p.value)
-                    editor.apply()
-                }
-
-                is Module.JVM2 -> {
-                    editor.putInt("javaModule2Page", jvm2p.value)
-                    editor.apply()
-                }
-
-                is Module.JVM3 -> {
-                    editor.putInt("javaModule3Page", jvm3p.value)
-                    editor.apply()
-                }
-
-                is Module.JSM1 -> {
-                    editor.putInt("jsModule1Page", jsm1p.value)
-                    editor.apply()
-                }
-
-                is Module.JSM2 -> {
-                    editor.putInt("jsModule2Page", jsm2p.value)
-                    editor.apply()
-                }
-
-                is Module.JSM3 -> {
-                    editor.putInt("jsModule3Page", jsm3p.value)
-                    editor.apply()
-                }
-
-                is Module.PYM1 -> {
-                    editor.putInt("pythonModule1Page", pym1p.value)
-                    editor.apply()
-                }
-
-                is Module.PYM2 -> {
-                    editor.putInt("pythonModule2Page", pym2p.value)
-                    editor.apply()
-                }
-
-                is Module.PYM3 -> {
-                    editor.putInt("pythonModule3Page", pym3p.value)
-                    editor.apply()
-                }
-
-                else -> {
-                    _currentPage.value
-                }
+            for (i in cModulesCurrentPage.indices) {
+                editor.putInt("cModulesCurrentPage_$i", cModulesCurrentPage[i])
             }
+            editor.apply()
         }
     }
 
@@ -300,33 +231,12 @@ class AppViewModel @Inject constructor() : ViewModel(), DefaultData {
     fun dataRestorer(context: Context) {
         viewModelScope.launch(Dispatchers.IO) {
             preferences = context.getSharedPreferences(preferenceName, Context.MODE_PRIVATE)
-            when (_currentState.value) {
-                is Current.KT -> {
-                    ktm1p.value = preferences.getInt("kotlinModule1Page", 1)
-                    ktm2p.value = preferences.getInt("kotlinModule2Page", 1)
-                    ktm3p.value = preferences.getInt("kotlinModule3Page", 1)
-                }
-
-                is Current.JV -> {
-                    jvm1p.value = preferences.getInt("javaModule1Page", 1)
-                    jvm2p.value = preferences.getInt("javaModule2Page", 1)
-                    jvm3p.value = preferences.getInt("javaModule3Page", 1)
-                }
-
-                is Current.JS -> {
-                    jsm1p.value = preferences.getInt("jsModule1Page", 1)
-                    jsm2p.value = preferences.getInt("jsModule2Page", 1)
-                    jsm3p.value = preferences.getInt("jsModule3Page", 1)
-                }
-
-                is Current.PY -> {
-                    pym1p.value = preferences.getInt("pythonModule1Page", 1)
-                    pym2p.value = preferences.getInt("pythonModule2Page", 1)
-                    pym3p.value = preferences.getInt("pythonModule3Page", 1)
-                }
-
-                else -> {}
+            val restoredArray = mutableListOf<Int>()
+            for (i in cModulesCurrentPage.indices) {
+                val value = preferences.getInt("cModulesCurrentPage_$i", 1)
+                restoredArray.add(value)
             }
+            cModulesCurrentPage = restoredArray
         }
     }
 }
