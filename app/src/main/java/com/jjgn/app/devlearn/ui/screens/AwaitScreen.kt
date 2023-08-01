117546da8dade2b536ed0ea1e84174ea7b65f383
velaz <juanma261005@gmail.com>
2023-08-01 19:08:17 -0400

    New loading screen

--- app/src/main/java/com/jjgn/app/devlearn/ui/screens/AwaitScreen.kt
+++ app/src/main/java/com/jjgn/app/devlearn/ui/screens/AwaitScreen.kt
@@ -1,15 +1,40 @@
 package com.jjgn.app.devlearn.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.jjgn.app.devlearn.R
 
 @Composable
 fun AwaitScreen() {
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Cargando...")
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Column(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()) {
+            Image(
+                painter = painterResource(id = R.drawable.undraw_loading_re_5axr),
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(bottom = 32.dp)
+                    .size(300.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text("Cargando...", modifier = Modifier.align(Alignment.CenterHorizontally))
+        }
     }
+
 }
