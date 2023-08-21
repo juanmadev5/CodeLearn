68d1be7ede90d4aca016b55eb6760916b9c1fe29
velaz <juanma261005@gmail.com>
2023-08-21 19:22:45 -0400

    Some settings is here

---
+++ app/src/main/java/com/jjgn/app/devlearn/data/AppSettings.kt
@@ -0,0 +1,14 @@
+package com.jjgn.app.devlearn.data
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+
+const val isSelectedKey = "isSelectedFirstC"
+const val isSelectedFirst = false
+const val awaitScreenMillis = 800
+const val awaitControllerDelay = 1500L
+const val backHandlerDelay = 1500L
+const val dsManagerDelay = 2000L
+const val dsDelay = 3000L
+const val defaultTextSize = 16
+val awaitScreenTSetting = fadeIn(animationSpec = tween(durationMillis = awaitScreenMillis))
