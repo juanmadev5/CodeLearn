b92b7844a3901410af58f1ccf517b6f787616422
velaz <juanma261005@gmail.com>
2023-09-23 12:17:21 -0400

    Minor changes

--- app/src/main/java/com/jjgn/app/devlearn/data/DataStoreModule.kt
+++ app/src/main/java/com/jjgn/app/devlearn/data/DataStoreModule.kt
@@ -9,12 +9,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
 
     @Provides
+    @Singleton
     fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
         return context.dataStore
     }
